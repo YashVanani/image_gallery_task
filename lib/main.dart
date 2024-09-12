2c8b7eb037570cdbf61e030b276793b4d9a3dd31
YashVanani <vananiyash9828@gmail.com>
2024-09-12 11:05:21 +0530

    - update

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:task_demo/ui/screen/image_gallrey_screen.dart';
+import 'package:news/screens/main/main_screen.dart';
+
+import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Gallery',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBgColor,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(backgroundColor: kPrimaryColor),
+        ),
+        textTheme: TextTheme(
+          // bodyText1: TextStyle(color: kBodyTextColor),
+          // bodyText2: TextStyle(color: kBodyTextColor),
+          // headline5: TextStyle(color: kDarkBlackColor),
+        ),
       ),
-      home: const ImageGalleryScreen(),
+      home: MainScreen(),
     );
   }
 }
